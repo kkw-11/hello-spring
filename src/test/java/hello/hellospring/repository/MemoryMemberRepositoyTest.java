bc3ee19815f37602c7dee11392a95ce12784ffe0
kiwon <kkw-11@naver.com>
2022-07-02 13:23:11 +0900

    test case findAll method

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoyTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoyTest.java
@@ -1,12 +1,22 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MemoryMemberRepositoyTest {
-    MemberRepository repository = new MemoryMemberRepository();
+import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class MemoryMemberRepositoyTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach()
+    public void afterEach(){
+        repository.clearStore();
+    }
     @Test
     public void save(){
         Member member = new Member();
@@ -20,9 +30,46 @@ class MemoryMemberRepositoyTest {
         System.out.println(result);
         System.out.println(member.getName());
 
-        Assertions.assertEquals(member,member);
+//        Assertions.assertEquals(member,member);
+        assertThat(member).isEqualTo(result);
 
+    }
+
+
+    @Test
+    public void findNyName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
 
 
     }
+
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+
+
 }
